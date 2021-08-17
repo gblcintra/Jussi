ce7f7fcb4f44cf6b3cdd92c244451539cb8d9a47
Gabriel Cintra <gabrieloliveiracintraa@hotmail.com>
2021-08-17 12:56:59 -0300

    feat(footer): create footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,38 @@
 import './index.scss';
 
+import Wpp_Company from '../../assets/wppcompany.svg';
+import Facebook from '../../assets/facebook.svg';
+import Instagram from '../../assets/instagram.svg';
+import Linkedin from '../../assets/linkedin.svg';
 
 export default function Footer() {
   return (
     <footer className="footer">
-      
+      <div className="footer__content">
+        <div className="footer__left">
+          <img src={Wpp_Company} alt="Wpp Company" />
+        </div>
+        <div className="footer__right">
+
+          <ul className="footer__social-media">
+            <li>
+              <a href="https://facebook.com">
+                <img src={Facebook} alt="Facebook" />
+              </a>
+            </li>
+            <li>
+              <a href="https://instagram.com">
+                <img src={Instagram} alt="Instagram" />
+              </a>
+            </li>
+            <li>
+              <a href="https://linkedin.com">
+                <img src={Linkedin} alt="Linkedin" />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </footer>
   );
 }
