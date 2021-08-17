b7fb0568c7a691fb763cd13a30ba4456ae6a77df
Gabriel Cintra <gabrieloliveiracintraa@hotmail.com>
2021-08-17 12:57:15 -0300

    feat(error): create page error

--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -1,20 +1,14 @@
 
-import {Link} from 'react-router-dom';
+import './index.scss' 
 
-export default function Erro() {
-  if (document.querySelector('.container').classList[1]) {
-    let classe = document.querySelector('.container').classList[1];
-    document.querySelector('.container').classList.remove(classe.toString())
-  }
-  document.querySelector('.container').classList.add('error');
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+export default function Error() {
   return (
-    <div>
-      <h1>Hum, parece que essa pagina nao existe!</h1><br/>
-
-      <span>Você pode estar procurando:</span> <br/>
-      <Link to="/" >Inicio</Link> <br/>
-      <Link to="/contato">Contatos</Link><br/>
-      <Link to="/sobre">Sobre</Link>
-    </div>
+    <>
+      <Header />
+      <div className="teste"><h1>Hum, parece que essa pagina nao existe!</h1></div>
+      <Footer />
+    </>
   );
- }
+}
