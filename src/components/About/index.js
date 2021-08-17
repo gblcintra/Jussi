62031aff8bedfe009adab1ba9a038a54e341f81f
Gabriel Cintra <gabrieloliveiracintraa@hotmail.com>
2021-08-17 19:03:24 -0300

    feat(about): create section about

---
+++ src/components/About/index.js
@@ -0,0 +1,28 @@
+
+import './index.scss'
+
+import Escritorio from '../../assets/escritorio-jussi.png';
+
+export default function Products() {
+
+  return (
+    <section className="about">
+      <div className="about__content">
+        <div className="about__description">
+          <div className="description__title">
+            <h2>Olá, somos a Jüssi</h2>
+          </div>
+          <div className="description__text">
+            <p>A Jüssi é uma agência integrante do grupo global WPP que vem há 10 anos consolidando o pensamento voltado para produtos e resolução de problemas. Nosso área dedicada exclusivamente para Produtos Digitais é organizada em 6 especialidades: Product Managamenet, User Experience Design, SEO, Tecnologia, Agile e User Behavior Analytics.</p>
+          </div>
+          <div className="description__button">
+            <button>Conheça a Jüssi</button>
+          </div>
+        </div>
+      </div>
+      <div className="about__image" >
+        <img src={Escritorio} />
+      </div>
+    </section>
+  )
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,11 +3,12 @@
 import { useEffect } from 'react'
 // import Orders from '../Orders';
 import Header from '../../components/Header';
-import Footer from '../../components/Footer';
 import Banner from '../../components/MainBanner';
-import Newsletter from '../../components/Newsletter';
-import Contact from '../../components/Contact';
 import Products from '../../components/Products';
+import About from '../../components/About';
+import Contact from '../../components/Contact';
+import Newsletter from '../../components/Newsletter';
+import Footer from '../../components/Footer';
 
 export default function Dashboard() {
   document.title = 'Home'
@@ -25,6 +26,7 @@ export default function Dashboard() {
         <Header />
         <Banner/>
         <Products/>
+        <About/>
         <Contact/>
         <Newsletter/>
         <Footer />
